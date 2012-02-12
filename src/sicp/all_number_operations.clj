0f625ce6d994262b2e62b08ec14475704728261b
Marc-Daniel Ortega <globulon@gmail.com>
2012-02-12 16:35:05 +0100

    ex2.83 is done as we can raise types from a tower of types

--- src/sicp/all_number_operations.clj
+++ src/sicp/all_number_operations.clj
@@ -8,19 +8,44 @@
          complex
          complex-polar
          complex-rectangular
-         rational]))
+         rational
+         real-number]))
 
-(def tower [clj-type rat-type complex-type])
+(def rectangular-datum (rectangular-complex))
+(def polar-datum (polar-complex))
 
-(defmulti raise (fn [x] (tag-in x)))
+(defmethod make-from-real-imag :real-imag-constructor [x y]
+  (make-complex (make-from-real-imag-constructor rectangular-datum x y)))
+
+(defmethod make-from-mag-ang :mag-ang-constructor [r theta]
+  (make-complex (make-from-mag-ang-constructor polar-datum r theta)))
+
+(defmulti raise (fn [data] (tag-in data)))
 
 (defmethod raise clj-type [x]
   (make-rat (clj-data x) 1))
 
-(defmethod raise clj-type [x]
-  (make-rat (clj-data x) 1))
+(defmethod raise rat-type [x]
+  (make-real (double (/ (numer x) (denum x)))))
 
+(defmethod raise real-type [x]
+  (make-from-real-imag (real-value x) 0))
 
+(def tower {clj-type 0 rat-type 1 real-type 2 complex-type 3})
+
+(defn- higher-type-in [& xs]
+  (first
+    (apply max-key val
+      (select-keys tower (map tag-in xs)))))
+
+(defn raise-to [target-type x]
+  (if (= target-type (tag-in x))
+    x
+    (recur target-type (raise x))))
+
+(defn raise-arguments [& xs]
+  (let [target-type (higher-type-in xs)]
+    (recur (raise-to target-type xs))))
 
 ;(defmethod add [complex-type clj-type] [x y]
 ;  (add ))
--- test/sicp/test/all_number_operations_spec.clj
+++ test/sicp/test/all_number_operations_spec.clj
@@ -2,17 +2,37 @@
   ^{:doc "supposedly complete exercise 2.83 and 2.84"}
   (:use clojure.test)
   (:use [sicp
+         core
          generic-arithmetic
          clojure-number
-         rational
          complex
+         complex-polar
+         complex-rectangular
+         rational
+         real-number
          all-number-operations]))
 
-(deftest raise-from-integer-shouldraise-to-rational
-  (is (= rat-type (tag-in (raise (make-clj 1))))))
+;(deftest add-integer-with-complex-should-create-a-new-complex
+;  (is (= (make-from-real-imag 3 4) (add (make-clj 1) (make-from-real-imag 2 4)))))
 
-;(deftest raise-from-rational-should-raise-to-complex
-;  (is (= complex-type (tag-in (raise (make-rat 1 2))))))
-;
+(deftest raise-should-promote-integer-to-rational
+  (is (= (make-rat 2 1) (raise (make-clj 2)))))
 
-;(run-tests )
+(deftest raise-should-promote-rational-to-real
+  (is (= (make-real 2.0) (raise (make-rat 2 1)))))
+
+(deftest raise-should-promote-real-to-complex
+  (is (= (make-from-real-imag 2 0)  (raise (make-real 2)))))
+
+(deftest raise-to-complex-integer-should-provide-complex-from-integer
+  (is (= (make-from-real-imag 2.0 0) (raise-to complex-type (make-clj 2)))))
+
+(deftest raise-to-rat-integer-should-provide-rat-from-integer
+  (is (= (make-rat 2 1) (raise-to rat-type (make-clj 2)))))
+
+(deftest raise-to-real-integer-should-provide-real-from-integer
+  (is (= (make-real 2.0) (raise-to real-type (make-clj 2)))))
+
+
+
+(run-tests)
